002c9b307e82d2b1c23add659ef57e00563d14da
Hermann KAO <38349706+HermannKK@users.noreply.github.com>
2020-01-25 12:21:02 +0100

    [DESIGN UPDATE] Login page added

--- public/index.php
+++ public/index.php
@@ -5,15 +5,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="css/bootstrap/bootstrap.min.css">
-    <script src="js/bootstrap/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="../css/bootstrap/bootstrap.min.css">
+    <script src="../js/bootstrap/bootstrap.min.js"></script>
     <title>Gestion de tickets</title>
 </head>
 
 <body>
     <h1>Public</h1>
-    <script src="js/jquery-3.4.1.slim.min.js"></script>
-    <script src="js/popper.min.js"></script>
+    <script src="../js/jquery-3.4.1.slim.min.js"></script>
+    <script src="../js/popper.min.js"></script>
 </body>
 
 </html>
