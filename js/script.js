b57de58db27f15fb597480baa3db37a248f5ac67
Hermann KAO <38349706+HermannKK@users.noreply.github.com>
2020-01-26 18:23:49 +0100

    [FRONT-END][Index.html] Update and added JS

--- js/script.js
+++ js/script.js
@@ -1,14 +1,24 @@
-function showPassword(e) {
-    if (e) {
-        document.getElementById("eyeIcon").innerHTML = '<i class="fa fa-eye"></i>';
+function showPassword() {
+    var input = document.getElementById("passswordBox");
+    if (input.type === "password") {
+        input.type = "text";
+        document.getElementById("eyeIcon").innerHTML = '<i class="fa fa-eye-slash"></i>';
+        document.getElementById("eyeIcon").title = "cacher le mot de passe";
     } else {
-        var input = document.getElementById("passswordBox");
-        if (input.type === "password") {
-            input.type = "text";
-            document.getElementById("eyeIcon").innerHTML = '<i class="fa fa-eye-slash"></i>';
-        } else {
-            input.type = "password";
-            document.getElementById("eyeIcon").innerHTML = '<i class="fa fa-eye"></i>';
-        }
+        input.type = "password";
+        document.getElementById("eyeIcon").innerHTML = '<i class="fa fa-eye"></i>';
+        document.getElementById("eyeIcon").title = "Aficher le mot de passe";
+    }
+}
+
+
+function handleConnection() {
+    submitButton = document.getElementById("submitButton");
+    if (this.state.isLoading) {
+        submitButton.innerHTML = '<div class="spinner-border text-primary"></div>';
+        submitButton.disabled = true;
+    } else {
+        submitButton.innerHTML = 'Connexion'
+        submitButton.disabled = fasle;
     }
 }
